2ea805f297b3f8ca635e83da4fcb57b44c04fd5c
DAWells <david.arthur.wells@gmail.com>
2024-07-09 16:59:38 +0100

    Add function to quantify how well aligned epitope matches sequence

--- examples/align_epitopes.py
+++ examples/align_epitopes.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from epimap import map
 
-
 #Define an example sequence and set of epitopes
 sequence = "abcdefghi"
 epitopes = pd.DataFrame(
@@ -21,3 +20,20 @@ map.float_peptides(epitopes)
 epitopes2 = epitopes.copy()
 epitopes2.columns = [c.upper() for c in epitopes2.columns]
 map.float_peptides(epitopes2, start_col="START", seq_col="SEQ")
+
+# Assess epitope alignment accuracy
+sequence = "abcdefghi"
+epitopes = pd.DataFrame(
+    {
+        'start': [1.0,2,3,4,6],
+        'end': [4,5,6,7,9],
+        'seq': ['abc','bcd','cde','def','fgh']
+    }
+)
+floating_epitopes = map.float_peptides(epitopes)
+epitope = floating_epitopes[-1]
+
+map.score_epitope_alignment(epitope, sequence)
+map.score_epitope_alignment(epitope, sequence, [" ", "-"])
+
+[map.score_epitope_alignment(epi, sequence)[0] for epi in floating_epitopes]
--- src/epimap/map.py
+++ src/epimap/map.py
@@ -17,3 +17,30 @@ def float_peptides(table, start_col="start", seq_col="seq"):
     floating_peptides = floating_peptides.tolist()
     return floating_peptides
 
+def score_epitope_alignment(epitope, sequence, gap="-", toupper=True):
+    """Proportion of aligned epitope positions that match the sequence
+
+    Args:
+        epitope (str): Aligned epitope sequence
+        sequence (str): Sequence epitopes are aligned to
+        gap (str, optional): Gap characters to ignore.
+            Use a list of strings to ignore multiple gap types. Defaults to "-".
+        toupper (bool, optional): Convert epitope and sequence to upper case before
+        comparison. Defaults to True.
+
+    Returns:
+        tuple: (score, matches)
+
+        - score (float): The proportion of non-gap epitope positions that match
+        the sequence.
+        - matches (list): List of booleans for matches of each non-gap position.
+    """
+    if toupper:
+        sequence = sequence.upper()
+        epitope = epitope.upper()
+    matches = []
+    for seqaa, epiaa in zip(sequence, epitope):
+        if epiaa not in gap:
+            matches.append(seqaa == epiaa)
+    score = sum(matches)/len(matches)
+    return score, matches
