04d711bbddc9d2578553492234e0a34550c2be96
DAWells <david.arthur.wells@gmail.com>
2024-07-31 15:40:38 +0100

    Exapand the simulate epitopes example

--- examples/simulate_epitopes.py
+++ examples/simulate_epitopes.py
@@ -3,7 +3,18 @@ import pandas as pd
 from epimap import map,stretch,utils
 
 sequence = utils.random_seq(10)
-epitopes = utils.random_epitopes(sequence, 9, (3,6), index=0, includeend=False)
+epitopes = utils.random_epitopes(sequence, 3, (3,6), index=0, includeend=False)
+stretch.stretch(epitopes)
+
+epitopes = utils.random_epitopes(sequence, 3, (3,6), index=1, includeend=False)
+stretch.stretch(epitopes)
+
+epitopes = utils.random_epitopes(sequence, 3, (3,6), index=0, includeend=True)
+stretch.stretch(epitopes)
+
+epitopes = utils.random_epitopes(sequence, 3, (3,6), index=1, includeend=True)
+stretch.stretch(epitopes)
+
+
 sequence
 epitopes
-
