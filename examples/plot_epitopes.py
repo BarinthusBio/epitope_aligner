72fd0bd6bbae075ceb759291c0e3fc6c94c79b1b
DAWells <david.arthur.wells@gmail.com>
2024-08-01 16:03:04 +0100

    Add grid plot example

--- examples/plot_epitopes.py
+++ examples/plot_epitopes.py
@@ -14,7 +14,8 @@ epitopes = pd.DataFrame(
     {
         'start': [1.0,2,2,6],
         'end': [3,5,3,8],
-        'seq': ['abc','bcde','bc','fgh']
+        'seq': ['abc','bcde','bc','fgh'],
+        'mhc_allele': ["x","x","y","z"]
     }
 )
 # Each epitope needs a length column to indicate how
@@ -35,8 +36,38 @@ plt.show()
 utils.plot_line(epitopes, y="length", c="black")
 plt.show()
 
-# If you define some matplotlib axes you can pass those
-fig,ax = plt.subplots(2)
+# If you define some matplotlib axes you can pass those to make a multi panel
+# figure. Set `sharex=True` to ensure positions line up between figures.
+fig,ax = plt.subplots(3, sharex=True, layout="constrained")
 ax[0].plot(positional_count)
 utils.plot_line(epitopes, y="length", c="black", ax=ax[1])
 plt.show()
+
+# Plot an epitope grid
+# Note that plt.matshow() discards the names of columns and rows
+# But we can set these as shown in the next example
+allele_position_count = stretched_epitopes.groupby(["mhc_allele", "position"]).size()
+grid = stretch.make_grid(
+    allele_position_count,
+    index=1,
+    seq_length=len(sequence),
+    empty_value=0
+)
+plt.matshow(grid)
+plt.show()
+
+
+# It is also important to set the aspect ration to "auto" for matshow()
+# We use .set_xticks to set the tick labels, it takes the tick number and
+# label.
+
+# Be careful when plotting grids with other figures. Even though we have
+# relabelled the ticks they are actually still zero indexed and so will
+# not align correctly with 1 indexed plots.
+fig,ax = plt.subplots()
+ax.matshow(grid)
+ax.set_aspect("auto")
+ax.set_xticks(np.arange(len(grid.columns)), grid.columns.get_level_values('position'))
+ax.set_yticks(np.arange(len(grid.index)), labels=grid.index)
+plt.show()
+
