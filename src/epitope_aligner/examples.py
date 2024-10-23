dc392e707219993ca6416e01730a5416cbbafe77
DAWells <david.arthur.wells@gmail.com>
2024-10-23 16:09:46 +0100

    Update examples links to cookbook

--- src/epitope_aligner/examples.py
+++ src/epitope_aligner/examples.py
@@ -2,10 +2,23 @@
 There are detailed examples demonstrating the main abilities of `epitope_aligner`.
 Links to all of them are listed below, including an intro to coordinate systems.
 
-- Primer on coordinate systems. [`index` and `includeend`](examples/coordinate_system.html)
-- Coordinates to sequence and back. [float](examples/floating_epitopes.html)
-- Analysing epitopes by position. [stretch](examples/stretch_epitopes.html)
-- Converting coordinates between aligned and unaligned sequences. [align](examples/align_coordinates.html)
+- [Quickstart]
+- [Cookbook](examples/cookbook.html) detailed discription and examples of all functions.
+    - [Install](examples/cookbook.html#install)
+    - [Background](examples/cookbook.html#background)
+        - [The big idea](examples/cookbook.html#the-big-idea)
+        - [Coordinate system](examples/cookbook.html#coordinate-system)
+        - [Naming conventions](examples/cookbook.html#naming-conventions)
+    - [`align_coords`](examples/cookbook.html#align-coordinates)
+    - [`unalign_coords`](examples/cookbook.html#unaligning-coordinates)
+    - [`float_epitopes`](examples/cookbook.html#float-epitopes)
+        - [Alignment accuracy](examples/cookbook.html#alignment-accuracy)
+    - [`locate_epitopes`](examples/cookbook.html#locate-epitopes)
+    - [Analysis](examples/cookbook.html#analysis)
+        - [`stretch`](examples/cookbook.html#stretch-epitopes)
+        - [`make_grid`](examples/cookbook.html#epitope-grids)
+    - [Utils](examples/cookbook.html#utils)
+    - [Conclusion](examples/cookbook.html#conclusion)
 
 This module exists to host the example jupyter notebooks.
 """
